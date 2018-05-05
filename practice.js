1ceddb8b476228aa6f0e646ceb9e7e787a3ad3c1
Taylor Jackson <tjack94@hotmail.com>
2018-05-04 18:40:23 -0600

    problem 5

--- practice.js
+++ practice.js
@@ -91,7 +91,15 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
+function evenFinder(nums){
+  newArray = []
+  for(var i = 0; i < nums.length; i++){
+    if(nums[i] % 2 == 0){
+      newArray.push(nums[i])
+    }
+  }
+  return newArray
+}
 
 ////////// EXTRA PRACTICE PROBLEMS BELOW //////////
 
